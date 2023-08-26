c06048c923345809665bb23600c826c912cc623a
Darcey Axon <darceyaxon@darceys-air.lan>
2023-08-25 21:27:38 -0700

    saving week 4 project updates

--- Week 4/codingproject.js
+++ Week 4/codingproject.js
@@ -87,8 +87,6 @@ console.log(averageNumberOfLettersName(names));
 console.log(namesConcatenate(names)); 
 
 
-
-
 /* 3. How do you access the last element of any array?
 */
 
@@ -99,3 +97,55 @@ console.log(names[names.length - 1]);
 */
 console.log(ages[0]); 
 console.log(names[0]); 
+
+/* 5. Create a new array called namesLength. Write a loop to iterate over the previously created names array 
+and add the length of each name to the nameLengths array.
+ Starting with names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'] create a new array with lengths. */
+
+ nameLengths = [];
+
+ for (let i = 0; i < names.length; i++) {
+  const lengths = names[i].length;
+  nameLengths.push(lengths); 
+ }
+
+ console.log(nameLengths); 
+
+ /* 6. Write a loop to iterate over the nameLengths array and calculate the sum of all the elements
+ in teh array. */
+
+ function sumOfNames(nameLengths) {
+  let sum = 0
+  for (i = 0; i < nameLengths.length; i++) {
+    sum += nameLengths[i]
+  } return sum; 
+ }
+ nameLengths = [];
+
+ for (let i = 0; i < names.length; i++) {
+  const lengths = names[i].length;
+  nameLengths.push(lengths); 
+ }
+
+ console.log(sumOfNames(nameLengths)); 
+
+  /* 7. Write a function that takes two parameters (word and n) as arguments and returns the word 
+  concatenated to itslef n number of times (ie if i pass in Hello and 3, expected return of HelloHelloHello). */
+
+ 
+
+function wordFunction(word, n) {
+    console.log(word.repeat(n)); 
+  }
+
+  wordFunction("Hello", 3); 
+
+
+
+  /* 8. Write a function that takes two parameters (as noted) and returns a full name. The full name
+  should be the first and the last name separated by a space. */
+
+
+let createFullName = (firstName, lastName) => `${firstName} ${lastName}`; 
+
+console.log(createFullName("Casa", "Azul"));
