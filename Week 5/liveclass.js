5b2014a459a3454d3befc4eb5b059f265b857cb2
Darcey Axon <darceyaxon@darceys-air.lan>
2023-09-11 21:03:17 -0700

    updating for week 7 class notes

--- Week 5/liveclass.js
+++ Week 5/liveclass.js
@@ -1,6 +1,24 @@
-class Zombie {
+/* class Human { 
+    constructor(name) {
+        this.alive = true;
+        this.name = name;
+
+    }
+
+}
+
+class Accountant extends Human {
+    constructor(name) {
+        super(this.name); 
+        this.alive = false; 
+    }
+}
+    
+    
+class Zombie extends Accountant {
     constructor(name) {
         this.name = name;
+
         this.alive = false;
     }
 
@@ -8,6 +26,21 @@ class Zombie {
         console.log(`${this.name} is eating ${food}`);
     }
 
+}
+
+/* class Weapon {
+    constructor() {
+        this.strength = randomNum(1,10); 
+    }
+
+    blowUp() {
+        console.log(`Self Destructing`); 
+    }
+
+    break() {
+        this.broken = true; 
+        this.amountToFix = this.strength; 
+    }
 }
 
 class Poison {
@@ -22,11 +55,71 @@ let aubrey = new Zombie('Aubrey');
 let zombies = [];
 
 
+Zombie Hunters
 
+Wearwolves
+
+Accountants
 
 function randomNum(min, max) {
     min = Math.cell(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 
+} */
+
+
+class Human {
+    constructor(name) {
+        this.alive = true;
+        this.name = name;
+    }
 }
+class Accountant extends Human {
+    constructor(name) {
+        super(name);
+        this.alive = false;
+        this.inventory = [];
+        
+    }
+    deathbyexplosion() {
+        if (this.alive) {
+            this.alive = false;
+            console.log(`${this.name} has met a fiery end due to an explosion`);
+        
+        } else {
+            console.log(`{$this.name} is already no more!`); 
+        }
+    }
+
+
+}
+class Zombie extends Accountant {
+    constructor(name) {
+        super(name);
+    }
+    eat(food) {
+        console.log(`${this.name} is eating ${food}`);
+    }
+}
+class Weapon {
+    constructor() {
+        this.strength = randomNum(1,10);
+    }
+    blowUp() {
+        console.log('Self destructing');
+    }
+    break() {
+        this.broken = true;
+        this.amountToFix = this.strength;
+        this.payToFix = () => {
+            this.broken = false;
+        }
+    }
+}
+class Poison extends Weapon {
+    constructor() {
+        super();
+    }
+}
+
---
+++ Week7/index.js
@@ -0,0 +1,4 @@
+console.log("testing"); 
+
+// alert("Watch out for exploding Moons!")
+
